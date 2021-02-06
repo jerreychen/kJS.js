0fe2a3d12d2d73aea20cbc1b014f7e5252bdca7a
Jerreychen <169435+jerreychen@users.noreply.github.com>
2021-02-06 21:59:03 +0800

    更新bug

--- kJS-core-1.0.0.js
+++ kJS-core-1.0.0.js
@@ -30,6 +30,7 @@
     let kJS = {
         version: 'v1.0.0'
     };
+    
     this.isObject = function(value) {
         return Object.prototype.toString.call(value) === "[object Object]";
     }
@@ -51,6 +52,10 @@
     this.isArray = function(value) {
         return Object.prototype.toString.call(value) === "[object Array]";
     }
+    this.isDefined = function(value) {
+        return !("undefined" == typeof value);
+    }
+
     this.extend = function(o, cfg) {
         for(let key in cfg) {
             if(!o[key]) {
@@ -161,7 +166,12 @@
         'pow': function(num) {
 		    return Math.pow(this, num);
         },
-		
+        'between': function(min, max, withBoundary) {
+            if(withBoundary) {
+                return this >= min && this <= max;
+            }
+            return this > min && this < max;
+        },
 		'fixed': function(n) {
 		    n = n > 0 ? n : 2;
 			let times = Math.pow(10, n);
@@ -306,17 +316,19 @@
             if (hrs < 24) {
                 return hrs + '小时前';
             }
+            if (hrs < 48) { 
+                return '昨天';
+            }
+            if( hrs < 72) { 
+                return '前天';
+            }
 
             let days = Math.floor(stamp / 86400);
-            if (days < 30) {
-                switch (days) {
-                    case 1:
-                        return '昨天';
-                    case 2:
-                        return '前天';
-                    default:
-                        return days + '天前';
-                }
+            if(days < 30) { 
+                return days + '天前';
+            }
+            if(days < 60) { 
+                return '一个月前';
             }
 
             return this.format('Y年M月D日 H:I');
@@ -467,45 +479,29 @@
             }
         },
 
-        'keys': function() {
-		    let obj = this;
-
-		    if(Object.keys) {
-		        return Object.keys(obj);
-            }
-
-		    let keys = [];
-		    for(let k in obj) {
-		        keys.push(k);
-            }
-		    return keys;
-        },
-
-        'each': function(fn) {
-            let obj = this;
-            for(let key in obj) {
-                if((fn && fn.call(obj, key, obj[key])) === true) {
-                    return;
-                }
-            }
-        },
-
         'findByKey': function (key) {
-            let result = false;
-
-            this.each(function (k, v) {
-                if (k === key) {
-                    result = v;
-                    return true;
+            let obj = this;
+            
+            for(let k in obj) {
+                if(key === k) {
+                    return obj[key]; 
                 }
-            });
+            }
 
-            return result;
+            return false;
+        },
+
+        'forEach': function(callback) {
+            let obj = this;
+
+            for(let k in obj) {
+                callback && callback.call(obj, obj[k], k);
+            }
         }
     });
 
 	this.extend(Function.prototype, {
-	    'interval': function (ts, completeFn) {
+	    'repeat': function (ts, completeFn) {
 	        let self = this;
 	        let cnt = 0;
 	        let itl = setInterval(function() {
@@ -526,37 +522,43 @@
         }
     });
 
-    this.pageScripts = {};
-    this.hashScriptsInPage = {};
-    this.hashPreRender = null;
+    /*******************************************************************/
+    /********* 区域分隔：以上是针对原生对象的扩展，以下是 kJS 的功能组件 ********/
+    /*******************************************************************/
 
+    /**
+     * @module url 地址解析
+     * @param {String} [url=null] url地址，如果不提供，默认为当前地址 
+     */
     kJS.uri = function(url) {
+        url = url || document.location.href;
+ 
+        // 创建临时元素 
         let a =  document.createElement('a');
-        a.href = url || document.location;
-
-        let protocol 	= a.protocol.replace(':','');
-        let hostName 	= a.hostname;
-        let port 		= parseInt(a.port || 80);
-        let queryString = (a.search || '').substr(1);
-        let hash		= (a.hash || '').replace('#','');
-        let path 		= (a.pathname || '').replace(/^([^\/])/,'/$1');
+        // 将 url 赋值给 a 元素的 href 属性
+        a.href = url;
 
         return {
-            protocol	: protocol,
-            host		: hostName,
-            port		: port,
-            path 		: path,
-            hash		: hash,
-            queryString : queryString,
+            base        : url.substr(0, url.indexOf('?')),
+            hash		: (a.hash || '').substr(1),
+            host		: a.hostname,
+            path 		: (a.pathname || '').replace(/^([^\/])/,'/$1'),
+            port		: parseInt(a.port || 80),
+            protocol	: a.protocol.replace(':',''),
+            queryString : (a.search || '').substr(1),
+            url         : url,
+
             /***
-             * 如果 key 为空，返回 querystring 的 key-value 数组
-             * 如果 key 不为空，返回 key 对应的值
-             * @param key
-             * @returns {*}
+             * 如果 key 为空，返回 querystring 的 key-value 数组；
+             * 如果 key 不为空，val 为空，返回 key 对应的值；
+             * 如果 key 不为空，val 不为空，则修改 key 对应的值
+             * @param {String} [key=null]
+             * @param {String} [val=null] 
+             * @returns {*} 返回单个key的值（String)，或者所有的 QueryString 参数 (Object)
              */
-            params		: function(key) {
+            params		: function(key, val) { 
                 let result = {},
-                    segs = queryString.split('&');
+                    segs = this.queryString.split('&');
 
                 for (let i = 0;i < segs.length; i++) {
                     if (!segs[i]) { continue; }
@@ -564,51 +566,77 @@
                     result[keyValuePair[0]] = keyValuePair[1];
                 }
 
-                if(key) {
-                    return result[key];
-                }
+                if(!key) { return result; }
+
+                if(!val) { return result[key]; }
+
+                result[key] = val; 
+
+                // QueryString 对象属性改变，同时更新 url 和 QueryString
+                this.queryString = kJS.util.buildQuery(result);
+                this.url = this.protocol + '://' + this.base
+                            + (this.port === 80 ? '' : this.port)
+                            + (this.queryString ? ('?' + this.queryString) : '')
+                            + (this.hash ? ('#' + this.hash) : '');
+
                 return result;
             },
-            /***
-             * 跳转到 newUrl
-             * @param newUrl
-             * @param backwards bool， 是否可以后退
-             */
-            redirect	: function(newUrl, backwards) {
-                if(backwards) {
-                    document.location.href = newUrl;
-                }
-                else {
-                    document.location.replace(newUrl);
-                }
-            }
-        };
-    }
 
+            /**
+             * 刷新当前 uri 地址
+             */
+            reload      : function() {
+                kJS.util.reload(this.base + '?' + this.queryString);
+            }
+        }
+    }
+    
     kJS.log = function(msg, label) {
         console.log((new Date()).format() + ' [' + (label || 'Log') + ']: ', msg);
     }
 
-    kJS.RegexHashRouter = (function () {
-        let win = this;
+    kJS.HashRouter = (function () {
+        let instance = null;
+        let hashScriptsInPage = null;
+        let preRenderList = null;
 
-        let popstateChanged = function(event) {
-            let hash = document.location.hash.substr(1);
+        function popstateChanged() {
+            let hash = kJS.uri().hash;
 
-            // 在 Hash 执行之前
-            win.hashPreRender && win.hashPreRender.call(this);
-
-            for (let hash_key in win.hashScriptsInPage) {
+            for (let hash_key in hashScriptsInPage) {
                 let result = (new RegExp(hash_key)).exec(hash);
                 if (result !== null) {
                     // callback 的 this 指向 正则匹配的结果
-                    let callback = win.hashScriptsInPage[hash_key];
+                    let pre_render = preRenderList[hash_key];
+                    pre_render && pre_render.call(result, hash, result.groups);
+
+                    let callback = hashScriptsInPage[hash_key];
                     callback && callback.call(result, hash, result.groups);
+
                     break;
                 }
             }
         }
 
+        function createInstance() {
+            hashScriptsInPage = {};
+            preRenderList = {};
+            currentHash = null;
+            
+            return {
+                _addHash: function(hash, callback) {
+                    currentHash = hash;
+                    hashScriptsInPage[hash] = callback;
+                },
+
+                beforeRender: function(callback) {
+                    if(isFunction(callback)) {
+                        preRenderList[currentHash] = callback;
+                    }
+                }
+            };
+        }
+ 
         return {
             /***
              * 注册 hash 和对应的 callback
@@ -616,25 +644,26 @@
              * @param callback
              */
             register: function (hash, callback) {
+
                 if (typeof hash !== 'string'
                     || hash.indexOf('^') !== 0 || hash.indexOf('$') !== hash.length - 1) {
                     throw ('Hash 格式错误，必须为 ^ 开头，$ 结尾的正则表达式字符串。');
                     return;
                 }
 
-                win.hashScriptsInPage[hash] = callback;
+                if (!instance) {  //  惰性创建实例
+                    instance = createInstance();
+                }
+                
+                instance._addHash(hash, callback);
+
+                return instance;
             },
-            preRender: function(callback) {
-                win.hashPreRender = callback;
-            },
-            /***
-             * 执行 hash 分发操作
-             * @param defaultHash
-             * @param callback
-             */
-            dispatch: function (defaultHash, callback) {
+
+            dispatch: function(defaultHash, callback) {
+                let hash = kJS.uri().hash;
                 // 配置默认跳转
-                if(!document.location.hash) {
+                if(!hash) {
                     document.location.hash = defaultHash;
                 }
                 else {
@@ -642,7 +671,7 @@
                 }
 
                 // 执行默认的回调，并以 hash 作为参数
-                callback && callback.call(this, document.location.hash.substr(1));
+                callback && callback.call(this, hash);
 
                 if (window.addEventListener) {
                     window.addEventListener('popstate', popstateChanged, false);
@@ -653,8 +682,23 @@
         }
     })();
 
-    kJS.PageJs = (function () {
-        let win = this;
+    /** 
+     * 向页面注册js
+     * 允许页面地址含有正则表达式
+     */
+    kJS.PageJS = (function () {
+        let instance = null;
+        let pageScripts = null;
+
+        function createInstance() {
+            pageScripts = {};
+
+            return {
+                _addPageJS: function (path, callback) {
+                    pageScripts[path] = callback;
+                }
+            }
+        }
 
         return {
             /***
@@ -662,16 +706,38 @@
              * @param path  path 是 location.path
              * @param callback
              */
-            ready: function (path, callback) {
-                win.pageScripts[path] = callback;
+            register: function (path, callback) {
+                if(!instance) {
+                    instance = createInstance();
+                }
+
+                instance._addPageJS(path, callback);
+
+                return instance;
             },
             /***
              * 根据当前的 location.path 执行对应的 javascript
              */
             run: function () {
-                let path = kJS.uri().path;
-                let callback = win.pageScripts[path];
-                callback && callback.call(this);
+                let path = kJS.uri().path; // 获取当前页面地址
+                let result = null;
+                
+                let callback = pageScripts[path];
+                
+                // 如果直接匹配的页面地址不存在，进行正则表达式查找
+                if(!callback) {
+                    for(let key in pageScripts) {
+                        result = new RegExp(key).exec(path);
+                        if( result !== null ) {
+                            path = key;
+                            break;
+                        } 
+                    }
+
+                    callback = pageScripts[path];
+                }
+                // 调用 callback
+                callback && callback.call(result, path, (result ? result.groups : null));
             }
         }
     })();
@@ -680,8 +746,180 @@
         let win = this;
 
         return {
+            /**
+             * 获取对象类型
+             * @param {*} value 
+             */
+            getType: function(value) {
+                let ret = Object.prototype.toString.call(value).match(/\[\w+\s(\w+)\]/);
+                if(ret) {
+                    return ret[1];
+                }
+                return '';
+            },
 
+            exec: function(func_string) {
+                if(isString(func_string)) {
+                    return new Function(func_string)();
+                }
+
+                return func_string;
+            },
+
+            callFunc: function() {
+                // 没有内容，或者第一个参数不是 String
+                if(arguments.length == 0 || !isString(arguments[0])) {
+                    return;
+                }
+                
+                // 第一个参数是函数名称
+                let funcName = arguments[0]; 
+
+                // 只有函数名，不带参数
+                if(/^[a-zA-Z0-9_]+$/.test(funcName)) {
+                    // 从第二个参数开始为需要执行函数的参数值
+                    let args = [];
+                    for(let i = 1; i < arguments.length; i++) {
+                        args.push(arguments[i]);
+                    }
+
+                    let func_args = []
+                    if(args.length > 0) {
+                        for(let i = 0; i < args.length; i++){ func_args.push('arg' + i); }
+                    }
+
+                    // 函数体
+                    let func_body = 'return ' + funcName + '(' + func_args.join(',') + ');';
+
+                    let fn = new Function(func_args, func_body);
+                    return fn.apply(window, args);
+                }
+
+                // 函数的格式 函数名() 或者 函数名(参数1,...)，带参数
+                if(/^[a-zA-Z0-9_]+\(.*?\)$/.test(funcName)) {
+                    return new Function('return ' + funcName)();
+                }
+            },
+
+            /**
+             * 组合 QueryString 
+             * @param {Object} qObj QueryString 对象
+             * @return {String} 返回组合后的 QueryString
+             */
+            buildQuery: function(qObj) {
+                let q = [];
+                for(k in qObj) {
+                    q.push(k + '=' + qObj[k]);
+                }
+                return q.join('&');
+            },
+
+            /**
+             * 重新加载
+             * @param {*} [qObj=null] 
+             */
+            reload: function(qObj) {
+                let uri = kJS.uri();
+
+                if(!qObj || !isString(qObj) && !isObject(qObj)) {
+                    window.location.replace(uri.url);
+                    return;
+                }
+
+                if(isString(qObj)) {
+                    window.location.replace(qObj);
+                    return;
+                }
+
+                let p = Object.assign(uri.params(), qObj || {});
+                window.location.replace(uri.base + '?' + this.buildQuery(p)); 
+            },
+
+            goBack: function() {
+                window.location.go(-1);
+            },
+
+            reloadBack: function() {
+                window.location.replace(document.referrer);
+            }
         }
+    })();
+
+    kJS.Autorun = (function() {
+        let pendingCssList = [];
+        let pendingJsList = [];
+    
+        function loadCss(cssFile) { 
+            let link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = cssFile;
+            document.head.appendChild(link);
+        }
+    
+        function loadJs(jsFile) {
+            return new Promise((resolve, reject) => {
+                let script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = jsFile;
+                document.body.appendChild(script);
+
+                script.addEventListener('load', ev => { // when the js execute done
+                    resolve();
+                });
+            });
+        } 
+    
+        return {
+            run: function(currentFileName, callback) {
+                
+                let jsElementList = document.getElementsByTagName('script');
+                let src = '';
+                
+                for(let i = 0; i < jsElementList.length; i++) {
+                    let jsElem = jsElementList[i];
+                    if(jsElem.src && jsElem.src.indexOf(currentFileName) > 0) {
+                        src = jsElem.src;
+                        break;
+                    }
+                }
+
+                if(!src) {
+                    return;
+                }
+
+                let uri = kJS.uri(src);
+
+                callback && callback.call({
+                    registerCss: function(cssFile) {
+                        pendingCssList.push(cssFile);
+                    },
+                    registerJs: function(jsFile, checkCallback) {
+                        pendingJsList[jsFile] = checkCallback;
+                    },
+                    jsReady: function(jsReadyCallback) {
+                        let jsList = [];
+
+                        pendingCssList.forEach(css => {
+                            loadCss(css);
+                        });
+
+                        for(let js in pendingJsList) {
+                            let fn = pendingJsList[js];
+
+                            if(fn && fn.call()) { 
+                                continue;
+                            }
+                            jsList.push(loadJs(js));
+                        }
+
+                        Promise.all(jsList).then(function() {
+                            jsReadyCallback && jsReadyCallback.call();
+                        });
+                    }
+                }, uri);
+            }
+        }
+
     })();
 
     return kJS;
